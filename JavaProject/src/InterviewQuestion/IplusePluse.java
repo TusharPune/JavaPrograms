35b3afc56a4a851cbdd6e7769ca131e86c4cb21c
Tushar Patil <tushar@Tushar-Patil>
2019-12-01 20:29:38 +0530

    New Java i ++ program

---
+++ JavaProject/src/InterviewQuestion/IplusePluse.java
@@ -0,0 +1,14 @@
+package InterviewQuestion;
+
+public class IplusePluse {
+
+	public static void main(String[] args) {
+		int i=0;
+		
+		i=i++; ///  i =0  --left  i++=1 --right
+		i=i++;//	i =0  --left  i++=1 --right
+		
+		System.out.println(i);
+	}
+
+}
