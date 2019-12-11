db41b6778f526609660fcc65051d817db6296c30
Tushar Patil <tushar@Tushar-Patil>
2019-12-11 15:29:13 +0530

    new program

---
+++ JavaProject/src/InterviewQuestion/StringManipulation.java
@@ -0,0 +1,23 @@
+package InterviewQuestion;
+
+public class StringManipulation {
+	
+	
+	
+	public static void main (String args[]) {
+		
+		
+		String Str= "Test java project with the help for selenium"; 
+		
+		//Find particular charater using index Numnber 
+		System.out.println(Str.charAt(6));
+		
+		
+		//Find particular charater  index using  String		
+		System.out.println(Str.indexOf("p"));
+		
+		
+		
+	}
+
+}
--- JavaProject/src/InterviewQuestion/secondlagraestnumber.java
+++ JavaProject/src/InterviewQuestion/secondlagraestnumber.java
@@ -6,13 +6,13 @@ public class secondlagraestnumber {
 
 	public static void main(String[] args) {
 		
-		int a[] = {8,8,5,4,6,7,6};	
+		int a[] = {0,1,2,1,1,5,1,0,1};
 		
 		Arrays.sort(a);		
 		System.out.println(a);
 		
 		for (int SorttedNumber:a) {
-//			System.out.print(SorttedNumber);		
+			System.out.print(SorttedNumber);		
 			
 		}
 		
