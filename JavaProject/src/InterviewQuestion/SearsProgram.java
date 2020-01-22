e1e542feb05566c736afd58b6d670c9ef6a2ebf1
Tushar Patil <tushar@Tushar-Patil>
2020-01-22 19:36:47 +0530

    SOrt program

---
+++ JavaProject/src/InterviewQuestion/SearsProgram.java
@@ -0,0 +1,29 @@
+package InterviewQuestion;
+
+public class SearsProgram {
+
+	public static void main(String[] args) {
+
+		String test = "";
+		String test2 = "";
+
+		String str[] = { "S", "E", "A", "R", "S" };
+		System.out.println(str.length);
+
+		for (int i = 0; i <= str.length - 1; i++) {
+			test = test + str[i];
+			System.out.println(test);
+		}
+
+		for (int i = str.length; i >= 1; i++) {
+//			System.out.println(test);
+			for (int j = 1; i <= i; j++) {
+				test2 = test2 + str[j];
+
+			}
+			System.out.println(test);
+
+		}
+	}
+
+}
