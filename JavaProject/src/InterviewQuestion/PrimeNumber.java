94bc8092b6df419e301e8a18e5fdcd2857eaefd2
Tushar Patil <tushar@Tushar-Patil>
2020-01-28 20:47:08 +0530

    hjgb

--- JavaProject/src/InterviewQuestion/PrimeNumber.java
+++ JavaProject/src/InterviewQuestion/PrimeNumber.java
@@ -2,4 +2,23 @@ package InterviewQuestion;
 
 public class PrimeNumber {
 
+	public static void main(String args[]) {
+
+		int num = 2;
+		int count = 0;
+
+		if (num > 1) {
+			for (int i = 1; i <= num; i++) {
+				if (num % i == 0)
+					count++;
+			}
+			if (count == 2) {
+				System.out.println(num + " PRIME NUMBER");
+			} else {
+				System.out.println(num + " NOT PRIME NUMBER");
+			}
+		} else {
+			System.out.println(num + " NOT A PRIME NUMBER");
+		}
+	}
 }
