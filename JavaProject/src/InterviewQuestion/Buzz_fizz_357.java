34360c03786263b05ef3dfb94e62afad3765414b
Tushar Patil <tushar@Tushar-Patil>
2020-01-28 20:00:45 +0530

    Words count

---
+++ JavaProject/src/InterviewQuestion/Buzz_fizz_357.java
@@ -0,0 +1,30 @@
+package InterviewQuestion;
+
+public class Buzz_fizz_357 {
+
+	public static void main(String[] args) {
+
+//		int a = 3;
+//
+//		if (a % 3 == 0 && a % 5 == 0) {
+//
+//			System.out.println("Buzz Fizz");
+//		}
+//		
+//		
+//		if (a%3==)
+//	}
+
+		
+		int a=0;
+		
+		int t=10;
+		int z=t/a; 
+		
+		
+		
+		
+		
+		
+}
+}
