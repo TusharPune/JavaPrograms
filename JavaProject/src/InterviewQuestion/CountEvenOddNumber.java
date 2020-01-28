af3fdd1f89bbb23475d8644ee2cac6ba617ef442
Tushar Patil <tushar@Tushar-Patil>
2020-01-28 20:12:52 +0530

    new Program

---
+++ JavaProject/src/InterviewQuestion/CountEvenOddNumber.java
@@ -0,0 +1,30 @@
+package InterviewQuestion;
+
+public class CountEvenOddNumber {
+	
+	
+	public static void main(String[] args)
+	{
+	     int num=7986123;
+	     int event_count=0;
+	     int odd_count=0;
+	     
+	     while(num>0)
+	     {
+	    	 int rem=num % 10;
+	    	 if (rem % 2==0)
+	    	  {
+				    event_count++;
+			} else 
+			{
+             odd_count++;
+			}
+	    	 num=num/10;
+	     }
+	     
+	     System.out.println("NUMBER OF EVEN NUMBERS :: "+event_count);
+	     System.out.println("NUMBER OF ODD NUMBERS :: "+odd_count);
+	}
+
+
+}
