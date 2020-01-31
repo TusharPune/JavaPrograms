16fc252c6e0eb7bbba868901931f6e29d2fbc527
Tushar Patil <tushar@Tushar-Patil>
2020-01-31 14:54:06 +0530

    new change for normal string and arrary string

--- JavaProject/src/Java/SearsProgram.java
+++ JavaProject/src/Java/SearsProgram.java
@@ -3,25 +3,22 @@ package Java;
 public class SearsProgram {
 
 	public static void main(String[] args) {
-
-		String test = "";
-		String test2 = "";
-
-		String str[] = { "S", "E", "A", "R", "S" };
-		System.out.println(str.length);
-
-		for (int i = 0; i <= str.length - 1; i++) {
-			test = test + str[i];
-			System.out.println(test);
+		
+		String str = "Yagna", nstr = " ", nstr2 = " ";
+		String strA[] = { "Y", "A", "G", "N", "A" };
+//		int lng = str.length();
+		int lng = strA.length;
+		for (int i = 0; i <= lng - 1; i++) {
+//			System.out.println(nstr = nstr + str.charAt(i));
+			System.out.println(nstr = nstr + strA[i]);
 		}
-
-		for (int i = str.length; i >= 1; i++) {
-//			System.out.println(test);
-			for (int j = 1; i <= i; j++) {
-				test2 = test2 + str[j];
-
+		for (int j = lng - 1; j >= 0; j--) {
+			for (int k = 0; k <= j - 1; k++) {
+//				nstr2 = nstr2 + str.charAt(k);
+				nstr2 = nstr2 + strA[k];
 			}
-			System.out.println(test);
+			System.out.println(nstr2);
+			nstr2 = " ";
 
 		}
 	}
