d117adadaf721f84bee474da935346853403e381
Tushar Patil <tushar@Tushar-Patil>
2020-04-05 10:28:40 +0530

    xfgd

--- JavaProject/src/Java/PrimeNumber.java
+++ JavaProject/src/Java/PrimeNumber.java
@@ -4,8 +4,9 @@ public class PrimeNumber {
 
 	public static void main(String args[]) {
 
-		int num = 2;
+		int num = 5;
 		int count = 0;
+		boolean isprime=true;
 
 		if (num > 1) {
 			for (int i = 1; i <= num; i++) {
@@ -20,5 +21,33 @@ public class PrimeNumber {
 		} else {
 			System.out.println(num + " NOT A PRIME NUMBER");
 		}
+//
+//		for (int i=2;i<=num/2;i++) {
+//			
+//			if(num%i==0) {
+//				
+//				isprime=false; 
+//				
+//				System.out.println("Given Number is not prime "+ i);
+//				
+//			}
+//			System.out.println("Given Number is prime "+ i);
+//			
+//			
+//			
+//			
+//		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
 	}
 }
