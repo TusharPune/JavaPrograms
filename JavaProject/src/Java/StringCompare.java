05a7aa8b422db4287ef01979190c2d5b70bee828
Tushar Patil <tushar@Tushar-Patil>
2020-08-17 14:51:35 +0530

    new text file reader

--- JavaProject/src/Java/StringCompare.java
+++ JavaProject/src/Java/StringCompare.java
@@ -10,12 +10,23 @@ public class StringCompare {
 		String str3= "JAVA TEST"; 
 		String str4= "JAVA";
 		Object obj= str1; 
+		String b ="225"; 
+		String c= new String("225");
 		
 		System.out.println(str1.compareTo(str2) ); //-32
 		System.out.println(str1.compareToIgnoreCase(str2));  //0
 		System.out.println(str1.compareTo(obj.toString())); //0
 		System.out.println(str1==str3);//true
-		System.out.println(str1==str4);//false
+		System.out.println(b.equals(c));//false
+		
+		
+		int a=5;
+		int b1=2;
+		int c1 = a/b1; 
+		
+		System.out.println(c1);
+		
+		
 		
 		
 		
---
+++ JavaProject/src/Java/TextFileReder.java
@@ -0,0 +1,25 @@
+package Java;
+
+import java.io.File;
+
+import java.io.IOException;
+
+import java.util.Scanner;
+
+public class TextFileReder {
+
+	public static void main(String[] args) throws IOException {
+
+		File file = new File("/home/tushar/test.txt");
+		Scanner sc = new Scanner(file);
+
+		while (sc.hasNextLine()) {
+			String sc2 = sc.nextLine();
+			String[] fName = sc2.split(",");
+			String result = fName[0];
+			System.out.println(result);
+			
+		}
+
+	}
+}
